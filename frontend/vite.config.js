956802a5f1e36f7c8eba7c254803be2edc5a817d
vipinyadav01 <snehagupta28102003@gmail.com>
2025-04-21 23:32:16 +0530

    feat(PWA): add service worker registration and update manifest configuration
    feat(PWA): add service worker registration and update manifest configuration
    
    Implement service worker registration to enable PWA functionality. Update the manifest file with detailed app information, icons, and theme colors. Simplify Vercel configuration and enhance Vite build settings for better PWA support. These changes improve the app's offline capabilities and user experience.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,12 +2,43 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
+const manifestForPlugIn = {
+  registerType: 'prompt',
+  includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+  manifest: {
+    name: 'TrueGrit',
+    short_name: 'TrueGrit',
+    description: 'Fitness Tracking Application',
+    theme_color: '#171717',
+    background_color: '#f0e7db',
+    display: "standalone",
+    scope: '/',
+    start_url: "/",
+    orientation: 'portrait',
+    icons: [
+      {
+        src: '/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png'
+      },
+      {
+        src: '/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png'
+      }
+    ]
+  }
+};
+
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({ registerType: 'autoUpdate' }),
-  ],
-  server: {
+  plugins: [react(), VitePWA(manifestForPlugIn)],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    sourcemap: false
+  },
+  server: process.env.NODE_ENV === 'development' ? {
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
@@ -16,5 +47,5 @@ export default defineConfig({
         secure: false,
       },
     },
-  },
+  } : undefined
 });
